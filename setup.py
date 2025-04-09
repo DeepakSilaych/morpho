d56641a267f42b5bc1c82ac6d9b8d5e0f276178b
deepak silaych <deepaksilaych@gmail.com>
2025-04-09 15:36:03 +0530

    updated name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="morpho",
+    name="morpho-tokenizer",
     version="0.1.0",
     author="Deepak Silaych",
     author_email="deepaksilaych@gmail.com",
